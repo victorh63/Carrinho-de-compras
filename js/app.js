9016f90bc71a84dc264b78ceb7f19535e17d4064
Victor <vichugo251106@hotmail.com>
2025-06-30 16:12:21 -0300

    Início das funções "adicionar" e "limpar" e criação das Lets nomeProduto e quantidade

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function adicionar() {
+    // PASSO A PASSO DA FUNÇÃO (lógica)
+
+    // recuperar valores: nome do produto, quantidade e valor
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+
+    // calcular o preço, o subtutal
+    // adicionar no carrinho
+    // atualizar valor total
+};
+
+function limpar() {
+
+};
