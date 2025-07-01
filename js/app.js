56b6607ed61d136961bff7cb7495fdeb13d12f28
Victor <vichugo251106@hotmail.com>
2025-06-30 23:37:52 -0300

    Criação da let preco

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function adicionar() {
     // PASSO A PASSO DA FUNÇÃO (lógica)
 
-    // recuperar valores: nome do produto, quantidade e valor
+    // 1- recuperar valores: nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value;
     // o split é usado para separar infos em um array, nos parâmetros se informa o que separa as informações e ele devolve o valor da
     // posição pedida (no caso 0)
@@ -9,9 +9,11 @@ function adicionar() {
     let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
 
-    // calcular o preço, o subtutal
-    // adicionar no carrinho
-    // atualizar valor total
+    let preco = quantidade.value * valorUnitario;
+
+    // 2- calcular o preço, o subtutal
+    // 3- adicionar no carrinho
+    // 4- atualizar valor total
 };
 
 function limpar() {
