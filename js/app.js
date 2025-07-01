3ed8e40079035019e53865407788d24c8308eca0
Victor <vichugo251106@hotmail.com>
2025-06-30 23:34:52 -0300

    Criação da let produto e valorUnitario e atualização da let nomeProduto

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@ function adicionar() {
     // PASSO A PASSO DA FUNÇÃO (lógica)
 
     // recuperar valores: nome do produto, quantidade e valor
-    let nomeProduto = document.getElementById('produto');
+    let produto = document.getElementById('produto').value;
+    // o split é usado para separar infos em um array, nos parâmetros se informa o que separa as informações e ele devolve o valor da
+    // posição pedida (no caso 0)
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
 
     // calcular o preço, o subtutal
