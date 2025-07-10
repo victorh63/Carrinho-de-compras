ed0d74f9b14dd3f2b3870e31cc7f39013afc25bc
Victor <vichugo251106@hotmail.com>
2025-07-10 19:54:38 -0300

    Criação da let Carrinho

--- js/app.js
+++ js/app.js
@@ -7,12 +7,16 @@ function adicionar() {
     // posição pedida (no caso 0)
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-
-    let preco = quantidade.value * valorUnitario;
+    let quantidade = document.getElementById('quantidade').value;
 
     // 2- calcular o preço, o subtutal
+    let preco = quantidade * valorUnitario;
+
     // 3- adicionar no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`;
     // 4- atualizar valor total
 };
 
